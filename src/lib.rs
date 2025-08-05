92a084c67d3f4de73d70a7daf5d76c5fd813770e
Martin Nordholts <martin.nordholts@codetale.se>
2025-08-05 19:37:50 +0200

    wip  2025-08-05T19:37:50+02:00

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn assert_or_bless(actual: impl AsRef<str>, snapshot_path: impl AsRef<std::path::Path>) {
+    assert_or_bless_if(
+        actual,
+        snapshot_path,
+        std::env::var("ASSERT_OR_BLESS") == Ok("bless".to_string()),
+    );
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn assert_or_bless_if(
+    actual: impl AsRef<str>,
+    snapshot_path: impl AsRef<std::path::Path>,
+    bless: bool,
+) {
+    let snapshot_path = snapshot_path.as_ref();
+    let actual = actual.as_ref();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    if bless {
+        // Write the current public API to the snapshot path
+        std::fs::write(snapshot_path, actual).unwrap_or_else(|err| {
+            panic!("Failed to write snapshot to `{:?}`: {err}", snapshot_path,)
+        });
+    } else {
+        // Assert that the current public API matches the snapshot
+        let expected = std::fs::read_to_string(snapshot_path).unwrap_or_else(|err| {
+            panic!("Failed to read snapshot from `{:?}`: {err}", snapshot_path,)
+        });
+        similar_asserts::assert_eq!(actual, expected);
     }
 }
