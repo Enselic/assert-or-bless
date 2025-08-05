621bd4c0e7a3d772ef830782fdae3de678e39b8c
Martin Nordholts <martin.nordholts@codetale.se>
2025-08-05 20:00:00 +0200

    wip  2025-08-05T20:00:00+02:00

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
-pub fn assert_or_bless(actual: impl AsRef<str>, snapshot_path: impl AsRef<std::path::Path>) {
-    assert_or_bless_if(
+pub fn assert_eq_or_bless(actual: impl AsRef<str>, snapshot_path: impl AsRef<std::path::Path>) {
+    assert_eq_or_bless_if(
         actual,
         snapshot_path,
         std::env::var("ASSERT_OR_BLESS") == Ok("bless".to_string()),
     );
 }
 
-pub fn assert_or_bless_if(
+pub fn assert_eq_or_bless_if(
     actual: impl AsRef<str>,
     snapshot_path: impl AsRef<std::path::Path>,
     bless: bool,
@@ -25,5 +25,35 @@ pub fn assert_or_bless_if(
             panic!("Failed to read snapshot from `{:?}`: {err}", snapshot_path,)
         });
         similar_asserts::assert_eq!(actual, expected);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_assert_eq(first: &str, second: &str) {
+        let test_dir = tempfile::tempdir().unwrap();
+        let snapshot_path = test_dir.path().join("test-snapshot.txt");
+
+        assert_eq_or_bless_if(first, &snapshot_path, true);
+        assert_eq_or_bless(second, &snapshot_path);
+    }
+
+    #[test]
+    fn assert_eq_succeeds() {
+        test_assert_eq(
+            "this is the\ncorrect snapshot contents",
+            "this is the\nWRONG snapshot contents",
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn assert_eq_fails() {
+        test_assert_eq(
+            "this is the\ncorrect snapshot contents",
+            "this is the\ncorrect snapshot contents",
+        );
     }
 }
