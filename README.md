ce67a4842d29696dc52e564934b27b8f40f4fa75
Martin Nordholts <martin.nordholts@codetale.se>
2025-08-06 20:12:01 +0200

    wip rename-to-snapshot-testiong 2025-08-06T20:12:01+02:00

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
-### Audit the code
-
-This crate is ~30 lines of code. Audit with the following one-liner, but make sure you follow to the [crates.io Data Access Policy](https://crates.io/data-access):
-
-```sh
-curl -H "User-Agent: $USER at $HOST" \
-     -L https://crates.io/api/v1/crates/assert-or-bless/0.1.3/download |
-         tar --extract --gzip --to-stdout | less
-```
-
 ### Usage
 
 Write a test:
@@ -15,20 +5,34 @@ Write a test:
 ```rs
 #[test]
 fn check_snapshot() {
-    let actual == "...";
-    assert_or_bless::assert_eq_or_bless(actual, "snapshot.txt");
+    let contents == "... contents to regression test ...";
+    snapshot_testing::assert_eq_or_update(contents, "snapshot.txt");
 }
 ```
 
-Bless current output (write the snapshot file):
+Update the snapshot file before the first test:
 
 ```sh
-ASSERT_OR_BLESS=bless cargo test
+UPDATE_SNAPSHOTS cargo test
 ```
 
-Assert current output (compare with existing snapshot file):
+Assert that current contents matches the contents of the snapshot file:
 
 ```sh
 cargo test
+```
+
+### Diffing Engine
+
+We use the same [excellent diffing engine](https://github.com/mitsuhiko/similar) that [insta](https://github.com/mitsuhiko/insta) is using. But since we allow custom snapshot file extensions we do not suffer from [Consider ways to avoid conflicts with Jest snapshots for external tools (like GitHub Linguist)](https://github.com/mitsuhiko/insta/issues/425). That bug makes GitHub diffs [horrible](https://github.com/trishume/syntect/pull/591/files#diff-f3d28a949326c02c2b2c85c024a030561f3b2521457621bb2550792460c3a2ec). Unfortunately the `.gitattributes` workaround [does not work](https://github.com/trishume/syntect/pull/592#issuecomment-3153214380).
+
+### Audit the Code
+
+This crate is ~10 lines of code. Audit with the following one-liner, but make sure you follow to the [crates.io Data Access Policy](https://crates.io/data-access):
+
+```sh
+curl -H "User-Agent: $USER at $HOST" \
+     -L https://crates.io/api/v1/crates/snapshot-testing/0.1.0/download |
+         tar --extract --gzip --to-stdout | less
 ```
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
-/// Asserts that `actual` matches the text-file snapshot at `snapshot_path`. If
-/// there is a diff the function will panic with a helpful diff that shows what
-/// changed.
+/// Asserts that `value` matches the snapshot at `snapshot_path`. If there is a
+/// diff the function will panic with a colorful diff that shows what changed.
 ///
-/// If `bless` is `true` then `actual` will be written to the snapshot file at
-/// `snapshot_path` instead of asserting that it matches.
-pub fn assert_eq_or_bless_if(
-    actual: impl AsRef<str>,
+/// If the env var `UPDATE_SNAPSHOTS` is set to `1`, `yes`, or `true`, the
+/// public API will be written to the snapshot file instead of being
+/// asserted to match.
+pub fn assert_eq_or_update(
+    value: impl AsRef<str>,
     snapshot_path: impl AsRef<std::path::Path>,
     bless: bool,
 ) {
     let snapshot_path = snapshot_path.as_ref();
-    let actual = actual.as_ref();
+    let actual = value.as_ref();
 
     if bless {
         std::fs::write(snapshot_path, actual)
@@ -19,75 +19,5 @@ pub fn assert_eq_or_bless_if(
         let expected = std::fs::read_to_string(snapshot_path)
             .unwrap_or_else(|err| panic!("Reading `{snapshot_path:?}`: {err}"));
         similar_asserts::assert_eq!(actual, expected);
-    }
-}
-
-/// Same as [`assert_eq_or_bless_if`] but allows you to use an environment
-/// variable to determine when to bless.
-///
-/// If the environment variable named `env_var_name` is set to `1`, `yes` or
-/// `true` then `actual` will be written to the snapshot file at `snapshot_path`
-/// instead of asserting that it matches with `actual`.
-pub fn assert_eq_or_bless_with_env_var(
-    actual: impl AsRef<str>,
-    snapshot_path: impl AsRef<std::path::Path>,
-    env_var_name: impl AsRef<str>,
-) {
-    assert_eq_or_bless_if(
-        actual,
-        snapshot_path,
-        std::env::var(env_var_name.as_ref()).is_ok_and(|s| s == "1" || s == "yes" || s == "true"),
-    );
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    struct DirGuard(std::path::PathBuf);
-    impl DirGuard {
-        fn new(path: std::path::PathBuf) -> Self {
-            std::fs::create_dir_all(&path).unwrap();
-            Self(path)
-        }
-    }
-    impl Drop for DirGuard {
-        fn drop(&mut self) {
-            let _ = std::fs::remove_dir_all(&self.0);
-        }
-    }
-
-    pub fn bless(actual: impl AsRef<str>, snapshot_path: impl AsRef<std::path::Path>) {
-        assert_eq_or_bless_if(actual, snapshot_path, true);
-    }
-
-    pub fn assert_eq(actual: impl AsRef<str>, snapshot_path: impl AsRef<std::path::Path>) {
-        assert_eq_or_bless_if(actual, snapshot_path, false);
-    }
-
-    fn test_assert_eq(first: &str, second: &str) {
-        let dir = DirGuard::new(
-            std::env::temp_dir().join(format!("assert-or-bless-{}", fastrand::u32(0..1_000_000))),
-        );
-        let snapshot_path = dir.0.join("test-snapshot.txt");
-        bless(first, &snapshot_path);
-        assert_eq(second, &snapshot_path);
-    }
-
-    #[test]
-    fn assert_eq_succeeds() {
-        test_assert_eq(
-            "this is the\ncorrect snapshot contents",
-            "this is the\ncorrect snapshot contents",
-        );
-    }
-
-    #[test]
-    #[should_panic]
-    fn assert_eq_panics() {
-        test_assert_eq(
-            "this is the\ncorrect snapshot contents",
-            "this is the\nWRONG snapshot contents",
-        );
     }
 }
